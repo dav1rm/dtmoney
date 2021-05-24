f16c119f9cab62de0e4638dfd52df1d930312725
Davi Rodrigues <davirodrigues8@gmail.com>
2021-05-24 15:13:22 -0300

    add route get and post to transactions

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Ganho da Bolsa',
+          type: 'deposit',
+          category: 'Bolsa',
+          amount: 1200,
+          createdAt: new Date('2021-05-03 07:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Pagamento do aluguel',
+          type: 'withdraw',
+          category: 'Despesas',
+          amount: 400,
+          createdAt: new Date('2021-05-10 15:30:00'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     });
   }
 });
