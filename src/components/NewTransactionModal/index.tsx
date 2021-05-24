f19954050a50dfa360a012cd354c3d30f5a5d220
Davi Rodrigues <davirodrigues8@gmail.com>
2021-05-24 09:58:44 -0300

    add new transaction modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,41 @@
+import ReactModal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+
+import { Container } from './styles';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button 
+        type="button"
+        className="react-modal-close"
+        onClick={onRequestClose}
+      >
+        <img src={closeImg} alt="Fechar"/>
+      </button>
+
+      <Container>
+        <h2>Cadastrar transação</h2>
+        
+        <input type="text" placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+        
+        <input type="text" placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </ReactModal>
+  )
+}
